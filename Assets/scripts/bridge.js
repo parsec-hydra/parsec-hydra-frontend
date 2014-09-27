55fe5176b547e8a3b0600378fcd782a14d044092
namhaid <satirical_satyr@mac.com>
2014-09-27 11:45:03 -0400

    Initialized the project.

---
+++ Assets/scripts/bridge.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+var host = 'http:///';
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function devices(state) {
+
+		return jQuery.ajax(host + '/devices/' + state);
+
+}
+
+function acceleration(device, coordinate) {
+
+	return jQuery.ajax(host + '/');
+
+}
+
+function inclination(device, coordinate) {
+
+	return jQuery.ajax(host + device + '/' + coordinate);
+}
+
+function beginGesture(device) {
+
+	
+}
+
